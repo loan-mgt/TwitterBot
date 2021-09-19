7b4e81f98c39c8f86f50355d11c0d0d8aed4df67
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2021-09-19 16:39:21 +0200

    clean file

--- Tweet.py
+++ Tweet.py
@@ -2,8 +2,8 @@ import tweepy
 import requests
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("R06IXvoGk7rcId2FOhWGyrWT7", "C0wzHSo2wfMrNGNXn25TMpyW9XpcZMAPSiVDEhO7pnxK7Cqk24")
-auth.set_access_token("1308495726238986240-BFXTtzrnIip8SRBjgP5Tq2SV0SpAMt", "XEl7h7jJK7woDLcp24VEfhmfBCVilvmUt1OTFGvP9GKUq")
+auth = tweepy.OAuthHandler("KEY", "SECRET_KEY")
+auth.set_access_token("TOKEN", "SECRET_TOKEN")
 
 # Create API object
 api = tweepy.API(auth)
